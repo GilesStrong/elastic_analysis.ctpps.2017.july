0593ba165686e685791370032d6fe6f1a99eaf1f
GilesStrong <giles.strong@outlook.com>
2017-08-16 14:13:37 +0100

    cut 2

--- DS-cr-angle140/parameters.h
+++ DS-cr-angle140/parameters.h
@@ -143,7 +143,7 @@ void Init_45t_56b()
 {
 	// analysis settings
 	anal.cut1_a = 1.; anal.cut1_c =  5.1E-4; anal.cut1_si = 4.9E-4;
-	anal.cut2_a = 1.; anal.cut2_c =  4.9E-4.; anal.cut2_si = 4.4E-5;
+	anal.cut2_a = 1.; anal.cut2_c =  4.9E-4; anal.cut2_si = 4.4E-5;
 
 	anal.cut3_c = -0.09; anal.cut3_si = 0.09;
 	anal.cut4_c = 0.22; anal.cut4_si = 0.05;
